50c80a00cd800925ed45a9e2fdb14aae1e578036
Techie5879 <aritrahtml@gmail.com>
2022-10-02 01:17:00 +0530

    added about route

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def predict():
     else: 
         return render_template("prediction.html", predictions=predictions)
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
