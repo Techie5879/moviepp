2ff6caf346f9f6fbef733c55cefa2402bdb8c6ac
Techie5879 <aritrahtml@gmail.com>
2022-10-09 15:19:37 +0530

    updated org movie and movie components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './navbar/Navbar'
 export { default as Form } from './form/Form'
 export { default as OrgMovie } from './orgmovie/OrgMovie'
+export { default as Movie } from './movie/Movie'
---
+++ src/components/movie/Movie.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './movie.css'
+
+
+
+const Movie = ({movid, title, poster}) => {
+  return (
+    <div className='rec_each'>
+        <a href= {`https://www.imdb.com/title/tt${movid}/`}><img src={`https://image.tmdb.org/t/p/w200${poster}`} alt="rec1" /></a>
+        <p>{title}</p>
+    </div>
+  )
+}
+
+export default Movie
