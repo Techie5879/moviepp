59f11cf15b23cffef079d0de147ce1eb599dfde2
Techie5879 <aritrahtml@gmail.com>
2022-10-09 15:19:06 +0530

    added release date, movie components

--- src/Predict.js
+++ src/Predict.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-import {Navbar} from './components'
+import {Movie, Navbar} from './components'
 import {OrgMovie} from './components'
 
 import './predict.css'
@@ -14,6 +14,7 @@ const Predict = () => {
   const [org_imdb, setOrg_imdb] = useState("");
   const [imdb_ids, setImdb_ids] = useState([]);
   const [recs_posters, setRecs_posters] = useState([]);
+  const [org_release, setOrg_release] = useState("");
 
 
 
@@ -39,10 +40,10 @@ const Predict = () => {
         setOrg_imdb(Object.values(data)[0]["imdbId"])
         
         setOrg_poster(Object.values(data[0])[1][0]["poster_path"])
-
+        
         const imdb_result_array = Object.values(data).slice(1, 6);
         // console.log(imdb_result_array);
-
+        
         
         // Get movie titles from IMDb
         function get_movie_results(movie_obj) {
@@ -51,17 +52,17 @@ const Predict = () => {
         const movies_results = imdb_result_array.map(get_movie_results)
         setMovies(movies_results);
         // console.log(movies)
-
-
+        
+        
         // Get IMDb Ids for the recommended movies
         const movie_imdbs = imdb_result_array.map(get_movie_imdbs)
-
+        
         function get_movie_imdbs (movie_obj) {
           return movie_obj["imdbId"]
         }
         // console.log(movie_imdbs)
         setImdb_ids(movie_imdbs);
-
+        
         setOverview(Object.values(data[0])[1][0]["overview"])
 
         function get_recs_images (movie_obj) {
@@ -69,6 +70,22 @@ const Predict = () => {
         }
         const recs_images = imdb_result_array.map(get_recs_images)
         setRecs_posters(recs_images)
+
+        // console.log(Object.values(data[0])[1][0]["release_date"]);
+
+        // Date conversion from YYYY-MM-DD to DD-MM-YYYY
+        function convertDate(dateString){
+          var p = dateString.split(/\D/g)
+          return [p[2],p[1],p[0] ].join("-")
+          }
+           
+          setOrg_release(convertDate(Object.values(data[0])[1][0]["release_date"]))
+
+
+
+
+
+
       })
     } 
     
@@ -86,12 +103,20 @@ const Predict = () => {
   return (
     <div>
         <Navbar />
-        <h1>Users who liked {org_title} also liked:</h1>
+        
         {/* <h2>{org_poster}</h2> */}
         
         <br />
-        <OrgMovie synopsis={overview} org_poster_path={org_poster} org_imdbId = {org_imdb} />
-        <a href= {`https://www.imdb.com/title/tt${imdb_ids[0]}/`}><p>{movies[0]}</p></a>
+        <OrgMovie synopsis={overview} org_poster_path={org_poster} org_imdbId = {org_imdb} release_date={org_release} org_title={org_title}/>
+        <h1>Users who liked {org_title} also liked:</h1>
+        <div className='recs'>
+          <Movie movid={imdb_ids[0]} title={movies[0]} poster={recs_posters[0]} />
+          <Movie movid={imdb_ids[1]} title={movies[1]} poster={recs_posters[1]} />
+          <Movie movid={imdb_ids[2]} title={movies[2]} poster={recs_posters[2]} />
+          <Movie movid={imdb_ids[3]} title={movies[3]} poster={recs_posters[3]} />
+          <Movie movid={imdb_ids[4]} title={movies[4]} poster={recs_posters[4]} />
+        </div>
+        {/* <a href= {`https://www.imdb.com/title/tt${imdb_ids[0]}/`}><p>{movies[0]}</p></a>
         <a href= {`https://www.imdb.com/title/tt${imdb_ids[1]}/`}><p>{movies[1]}</p></a>
         <a href= {`https://www.imdb.com/title/tt${imdb_ids[2]}/`}><p>{movies[2]}</p></a>
         <a href= {`https://www.imdb.com/title/tt${imdb_ids[3]}/`}><p>{movies[3]}</p></a>
@@ -100,7 +125,7 @@ const Predict = () => {
         <img src={`https://image.tmdb.org/t/p/w200${recs_posters[1]}`} alt="rec2" />
         <img src={`https://image.tmdb.org/t/p/w200${recs_posters[2]}`} alt="rec3" />
         <img src={`https://image.tmdb.org/t/p/w200${recs_posters[3]}`} alt="rec4" />
-        <img src={`https://image.tmdb.org/t/p/w200${recs_posters[4]}`} alt="rec5" />
+        <img src={`https://image.tmdb.org/t/p/w200${recs_posters[4]}`} alt="rec5" /> */}
     </div>
   )
 }
