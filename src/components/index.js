46d3343a1c3abddbc8a76f3cf32a4e75587c3297
Techie5879 <aritrahtml@gmail.com>
2023-06-29 14:56:16 +0530

    website deploy to gh-pages

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './navbar/Navbar'
-export { default as Form } from './form/Form'
-export { default as OrgMovie } from './orgmovie/OrgMovie'
-export { default as Movie } from './movie/Movie'
